2d5890c48bfeffdf450ce39c8003be9611f5723e
Gielzy-Boateng <desmondboateng483@gmail.com>
2024-11-18 16:42:27 +0000

    feat:add coaches

--- src/components/home/staff.tsx
+++ src/components/home/staff.tsx
@@ -5,16 +5,16 @@ import Image from "next/image";
 const Staff = () => {
   const staff = [
     {
-      name: "John Doe",
-      image: "/assets/dreamer3.svg",
+      name: "CHIEF AFOAKWAH OBENG",
+      image: "/assets/coach-chairman.jpg",
     },
+    // {
+    //   name: "John Doe",
+    //   image: "/assets/assistant.jpg",
+    // },
     {
-      name: "John Doe",
-      image: "/assets/dreamer4.svg",
-    },
-    {
-      name: "John Doe",
-      image: "/assets/dreamer2.svg",
+      image: "/assets/head-coach.jpg",
+      name: "ALBERT ASARE",
     },
   ];
   return (
@@ -23,15 +23,15 @@ const Staff = () => {
         <h2 className="text-3xl lg:text-4xl text-center uppercase text-white">
           COACHING STAFF
         </h2>
-        <div className="grid grid-cols-1 max-w-[400px] lg:max-w-full mx-auto lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 max-w-[400px] lg:max-w-[70%]  mx-auto lg:flex lg:items-center lg:justify-center gap-6">
           {staff.map((item, index) => (
             <div key={index} className="relative">
               <Image
                 alt=""
                 src={item.image}
-                width={1000}
+                width={800}
                 className="w-full"
-                height={1000}
+                height={800}
               />
               <div className="absolute bottom-0 w-full left-0 p-4 ">
                 <div className="bg-white w-full p-4 rounded-full text-center uppercase font-semibold text-primary">
